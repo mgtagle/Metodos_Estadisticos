a6eddabda7867bba41854e36bc47e6d32f00fd0f
Marco Gonzalez Tagle <marco.gonzaleztg@uanl.edu.mx>
2022-09-19 08:05:07 -0600

    Update encuesta

--- Encuesta_script.R
+++ Encuesta_script.R
@@ -13,6 +13,7 @@ Enc1$Carrera <- as.factor(Enc1$Carrera)
 levels(Enc1$Carrera)
 Enc1$Semestre <- as.factor(Enc1$Semestre)
 levels(Enc1$Semestre)
+Enc1$oi_4a <- as.factor(Enc1$oi_4a)
 
 #  Función para transformar caracteres a fechas
 
@@ -45,9 +46,11 @@ range(Enc1$Edad)
 
 # ¿Cómo te entersaste de la facultad?
 
+paleta <- c("red", "gray", "blue", "yellow", "green")
 conFCF <- table(Enc1$oi_1)
 prop.table(conFCF)*100
-pie(prop.table(conFCF)*100)
+pie(prop.table(conFCF)*100,
+    col=palette("Tableau 10"))
 
 # ¿Fué tu primera opción la FCF?
 op <- table(Enc1$oi_2)
@@ -56,3 +59,37 @@ prop.table(op)*100
 # ¿Presentaste en otra facultad?
 of <- table(Enc1$oi_2a)
 prop.table(of)*100
+
+# Estuviste inscrito en otra facultad
+oi_4 <- table(Enc1$oi_4)
+prop.table(oi_4)*100
+
+# Donde etaba inscrito
+oi_4a <- table(Enc1$oi_4a)
+prop.table(oi_4a)*100
+oi_4a
+
+# cONVIVENCIA
+ef_1 <- table(Enc1$ef_1)
+prop.table(ef_1)*100
+
+ins1 <- table(Enc1$ins_1)
+prop.table(ins1)*100
+
+ap_1 <- table(Enc1$ap_1)
+prop.table(ap_1)*100
+
+ap_2 <- table(Enc1$ap_2)
+prop.table(ap_2)*100
+
+
+ap_3 <- table(Enc1$ap_3)
+prop.table(ap_3)*100
+
+
+# Guardar la BD con los datos nuevos en formato csv
+write.csv(Enc1, "Encuesta_codificada.csv")
+
+
+# paleta de colores
+# https://estadisticamente.com/paletas-de-colores-en-r/
